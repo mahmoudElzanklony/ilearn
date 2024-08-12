deaa0e558094df38661b31bd83c0d1a70be2ac13
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-12 21:07:51 +0300

    update subscriptions

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use App\Services\StreamImages;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 
 class ImageResource extends JsonResource
 {
@@ -13,13 +15,25 @@ class ImageResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+    public function tem_url($path)
+    {
+        $expires = now()->addMinutes(60);
+
+        $url = Storage::disk('wasabi')
+            ->temporaryUrl($path, $expires, [
+            'ResponseContentType' => 'application/octet-stream',
+            // Additional headers or options can be added here
+        ]);
+        return $url;
+    }
     public function toArray(Request $request): array
     {
         return [
             //'id'=>$this->id,
             //'imageable_type'=>$this->imageable_type,
             //'name'=>env('cloud_storage').(env('WAS_STATUS') == 1 ? '/' :'/images/').($this->name ?? 'default.png'),
-            'name'=>StreamImages::stream($this->name),
+            //'name'=>StreamImages::stream($this->name),
+            'name'=>$this->tem_url($this->name)
         ];
     }
 }
