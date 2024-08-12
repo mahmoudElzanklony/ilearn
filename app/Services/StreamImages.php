30fccf3aa629bea7ab9e452486a3904516085da4
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-13 00:38:24 +0300

    update subscriptions

--- app/Services/StreamImages.php
+++ app/Services/StreamImages.php
@@ -2,10 +2,39 @@
 
 namespace App\Services;
 
+use App\Models\subjects_videos;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class StreamImages
 {
+    public static function video_stream($filePath)
+    {
+
+
+
+        if (!Storage::disk('wasabi')->exists($filePath)) {
+            return response()->json(['error' => 'File not found'], 404);
+        }
+
+        $size = Storage::disk('wasabi')->size($filePath);
+        $mimeType = Storage::disk('wasabi')->mimeType($filePath);
+        $stream = Storage::disk('wasabi')->readStream($filePath);
+
+        $headers = [
+            'Content-Type' => $mimeType,
+            'Content-Length' => $size,
+            'Accept-Ranges' => 'bytes',
+            'Content-Disposition' => 'inline; filename="' . basename($filePath) . '"',
+        ];
+
+        return new StreamedResponse(function () use ($stream) {
+            while (ob_get_level()) {
+                ob_end_flush();
+            }
+            fpassthru($stream);
+        }, 200, $headers);
+    }
     public static function stream($path)
     {
         if (!Storage::disk('wasabi')->exists($path)) {
