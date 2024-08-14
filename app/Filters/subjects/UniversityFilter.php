6286dfa2c826fc2b6ad65a2e1a59e2c6296087ea
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-14 13:14:56 +0300

    update university filter

--- app/Filters/subjects/UniversityFilter.php
+++ app/Filters/subjects/UniversityFilter.php
@@ -8,8 +8,8 @@ class UniversityFilter extends FilterRequest
 {
     public function handle($request, Closure $next){
         if(request()->has('university_id')){
-            return $next($request)->whereHas('subject',fn($e)=>
-                $e->whereHas('category',fn($q)=>$q->where('university_id','=',request('university_id')))
+            return $next($request)->whereHas('subject.category',fn($e)=>
+                $e->where('university_id','=',request('university_id'))
             );
         }
         return $next($request);
