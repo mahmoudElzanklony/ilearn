6a2bb30a6665439fd0f4efa6f10477e13da29848
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-17 23:54:43 +0300

    add subscription university of user

--- app/Http/Requests/billFormRequest.php
+++ app/Http/Requests/billFormRequest.php
@@ -26,7 +26,7 @@ class billFormRequest extends FormRequest
             'start_date'=>'required|date',
             'end_date'=>'required|date',
             //'profit'=>'required|numeric',
-            'remain'=>'required|numeric',
+            'remain'=>'required|numeric|min:0',
             'note'=>'nullable',
         ];
     }
