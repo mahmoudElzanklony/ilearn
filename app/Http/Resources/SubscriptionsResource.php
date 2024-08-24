1368f18e4a731b7c35ddd6e5e12973a7dbb9cb00
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-25 02:45:45 +0300

    add

--- app/Http/Resources/SubscriptionsResource.php
+++ app/Http/Resources/SubscriptionsResource.php
@@ -34,9 +34,10 @@ class SubscriptionsResource extends JsonResource
 
         ];
         if(request()->filled('web')){
-            $arr['total_videos_per_subject']=$video_base_query->clone()->where('subject_id','=',$this->subject_id)->count();
+          $arr['total_videos_per_subject']=$video_base_query->clone()->where('subject_id','=',$this->subject_id)->count();
           $arr['total_seen_per_user']=$user_view_base_query->clone()
                 ->whereHas('video',fn($e)=> $e->where('subject_id','=',$this->subject_id))
+                ->where('user_id','=',$this->user_id)
                 ->count();
         }
         return $arr;
