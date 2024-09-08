59400c89065fd0330c43ac2a49fc28aa2ac03929
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-09-08 16:40:58 +0300

    edit message of whatApp

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
 
     public function login()
     {
-        $data = ['phone'=>request('phone'),'password'=>request('password')];
+        $data = ['phone'=>request('phone'),'password'=>request('password'),'deleted_at'=>null];
         if(auth()->attempt($data)){
             // check ip
             $user = User::query()->where('phone',$data['phone'])->first();
