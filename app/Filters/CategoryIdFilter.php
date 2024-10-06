82a2a1d6a2731591de4d70bce8cafb0215d41753
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-10-06 16:49:08 +0300

    edit of subscription

--- app/Filters/CategoryIdFilter.php
+++ app/Filters/CategoryIdFilter.php
@@ -1,8 +1,29 @@
 <?php
 
 namespace App\Filters;
-
-class CategoryIdFilter extends FilterRequest
+use App\Models\categories;
+use Closure;
+class CategoryIdFilter
 {
+    public function handle($request, Closure $next)
+    {
+        if(request()->filled('all_subjects') && request()->filled('category_id')){
+            return $next($request)->whereHas('subject.category', function ($query) {
+                $categoryId = request('category_id');
 
+                // Find the university_id for the given category
+                $universityId = categories::where('id', $categoryId)->value('university_id');
+
+                // Get all subjects that belong to categories of the same university
+                $query->whereHas('category', function ($subQuery) use ($universityId) {
+                    $subQuery->where('university_id', $universityId);
+                });
+            });
+        }else if (request()->filled('category_id')) {
+            return $next($request)->whereHas('subject.category', function ($e) {
+                $e->where('id','=',request('category_id'));
+            });
+        }
+        return $next($request);
+    }
 }
