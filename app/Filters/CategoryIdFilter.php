fde09a4a4efe388d2738b9a23604943d5d7e9dec
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-10-06 16:50:12 +0300

    edit of subscription

--- app/Filters/CategoryIdFilter.php
+++ app/Filters/CategoryIdFilter.php
@@ -8,14 +8,14 @@ class CategoryIdFilter
     public function handle($request, Closure $next)
     {
         if(request()->filled('all_subjects') && request()->filled('category_id')){
-            return $next($request)->whereHas('subject.category', function ($query) {
+            return $next($request)->whereHas('category', function ($query) {
                 $categoryId = request('category_id');
 
                 // Find the university_id for the given category
                 $universityId = categories::where('id', $categoryId)->value('university_id');
 
                 // Get all subjects that belong to categories of the same university
-                $query->whereHas('category', function ($subQuery) use ($universityId) {
+                $query->whereHas('university', function ($subQuery) use ($universityId) {
                     $subQuery->where('university_id', $universityId);
                 });
             });
