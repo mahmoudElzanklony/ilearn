2934c2fd90fd1e01589595b0ca9a0af9735c0f26
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-04 02:51:00 +0200

    generate wasbi url from cron job

--- app/Jobs/GenerateExpiringWasabiUrls.php
+++ app/Jobs/GenerateExpiringWasabiUrls.php
@@ -39,7 +39,7 @@ class GenerateExpiringWasabiUrls implements ShouldQueue
             $expiration = Carbon::now()->addHours(11);
             $temporaryUrl = Storage::disk('wasabi')
                 ->temporaryUrl($image->name, $expiration);
-
+            dd($image);
             // Update the wasbi_url column in the database
             $image->wasbi_url = $temporaryUrl;
             dd($image);
