028705448a3d2f4c42051bc6130fa64276e98f1a
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-04 03:00:59 +0200

    generate wasbi url from cron job

--- app/Jobs/GenerateExpiringWasabiUrls.php
+++ app/Jobs/GenerateExpiringWasabiUrls.php
@@ -29,7 +29,7 @@ class GenerateExpiringWasabiUrls implements ShouldQueue
      */
     public function handle(): void
     {
-
+        dd('start of generation');
         // Assuming your images are stored with Wasabi storage
         $images = images::query()->get();
         $videos = subjects_videos::query()->get();
