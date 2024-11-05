a5bc779319f019b719025142234001d50c4379ea
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-05 22:22:16 +0200

    add search name to subscriptions

--- app/Filters/subscriptions/UserNameFilter.php
+++ app/Filters/subscriptions/UserNameFilter.php
@@ -9,7 +9,7 @@ class UserNameFilter
     public function handle($request, Closure $next){
         if(request()->filled('name')){
             return $next($request)->whereHas('user',function($e){
-                $e->where('usernamesx','LIKE','%'.request('name').'%');
+                $e->where('users.username','LIKE','%'.request('name').'%');
             });
 
         }
