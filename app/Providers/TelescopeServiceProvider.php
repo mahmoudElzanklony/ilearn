e4570e10fc057abe2c03be4e2a67993bacfba145
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-19 20:12:52 +0200

    monitor telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -58,7 +58,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
-            return auth()->user()?->type == 'admin';
+            return true;
         });
     }
 }
