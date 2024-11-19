1103de67f1c082464efaef418ea7c6d82ff327d8
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-19 19:02:25 +0200

    monitor telescope

--- app/Http/Controllers/WebLoginController.php
+++ app/Http/Controllers/WebLoginController.php
@@ -15,9 +15,9 @@ class WebLoginController extends Controller
         if(request()->filled('phone') && request()->filled('password')){
             $user = User::query()
                 ->where('phone','+'.request('phone'))
-             //   ->where('password',request('password'))
+                ->where('password',request('password'))
                 ->first();
-            return $user;
+            return request()->all();
             if($user && $user->type == 'admin'){
                 auth()->login($user);
                 return 'login success';
