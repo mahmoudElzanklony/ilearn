26a9573ccfecf3fd6f7b12c39165a77da07b9064
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-12-26 23:30:50 +0200

    edit connection

--- config/database.php
+++ config/database.php
@@ -61,8 +61,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_PERSISTENT => true,
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET SESSION max_connections=200;',
+
             ]) : [],
         ],
 
