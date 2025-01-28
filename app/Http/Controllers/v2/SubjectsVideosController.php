f83750b636e1b52861350229f5045434209e6885
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-01-28 21:43:14 +0200

    add filter type for files

--- app/Http/Controllers/v2/SubjectsVideosController.php
+++ app/Http/Controllers/v2/SubjectsVideosController.php
@@ -92,6 +92,7 @@ class SubjectsVideosController extends SubjectsVideosControllerResource
 
             if($extension == 'pdf'){
                 $name = time().rand(0,9999999999999). '_file.' . $data['video']->getClientOriginalExtension();
+                dd(is_dir(storage_path('app/tmp/')));
                 exec("ffmpeg -i ".$data['video']."  ".storage_path('app/tmp/')."$name");
                 VideoQualities::save_at_wasabi($name,storage_path('app/tmp/').$name,'pdfs');
                 $data['video'] = $name;
