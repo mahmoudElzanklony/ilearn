a024d9d929a0dc15cb71356f1602e3a89618a235
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-01-28 20:47:36 +0200

    add filter type for files

--- app/Actions/v2/VideoQualities.php
+++ app/Actions/v2/VideoQualities.php
@@ -85,6 +85,7 @@ class VideoQualities
 
     public static function save_at_wasabi($name,$compressedFilePath,$start_path_wasbi = 'videos')
     {
+        dd($compressedFilePath);
         if(env('WAS_STATUS') == 1){
             // Set up AWS S3 Client with Wasabi credentials
             $s3Client = new S3Client([
