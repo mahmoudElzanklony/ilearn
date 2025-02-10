2214132992c227cc0877a9543920e67cc1147c36
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-10 22:17:28 +0200

    add base url cdn env

--- app/Jobs/GenerateExpiringWasabiUrls.php
+++ app/Jobs/GenerateExpiringWasabiUrls.php
@@ -61,7 +61,7 @@ class GenerateExpiringWasabiUrls implements ShouldQueue
         $perPage = 10;
 
         do {
-            $videos = subjects_videos::query()->paginate($perPage, ['*'], 'page', $page);
+            $videos = subjects_videos::query()->with('qualities')->paginate($perPage, ['*'], 'page', $page);
             Log::info('start build new urls');
             foreach ($videos as $video) {
                 $expiration = Carbon::now()->addHours(11);
@@ -74,9 +74,21 @@ class GenerateExpiringWasabiUrls implements ShouldQueue
                     $video->wasbi_url = $temporaryUrl;
                     $video->save();
                 }
+                foreach ($video->qualities as $quality) {
+                    $filePath = 'videos/' . $quality->name;
+                    if (Storage::disk('wasabi')->exists($filePath)) {
+                        $temporaryUrl = Storage::disk('wasabi')
+                            ->temporaryUrl($filePath, $expiration);
+
+                        $quality->wasbi_url = $temporaryUrl;
+                        $quality->save();
+                    }
+                }
             }
 
             $page++;
         } while ($videos->hasMorePages());
     }
+
+
 }
