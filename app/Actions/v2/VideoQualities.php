07db47a1ffc4090f690854b959f540ca922dc772
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-13 16:20:54 +0200

    test video upload

--- app/Actions/v2/VideoQualities.php
+++ app/Actions/v2/VideoQualities.php
@@ -7,6 +7,7 @@ use FFMpeg\Coordinate\Dimension;
 use FFMpeg\FFMpeg;
 use FFMpeg\Format\Video\X264;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 
 class VideoQualities
 {
@@ -24,7 +25,9 @@ class VideoQualities
             '360p' => ['width' => 640, 'height' => 360, 'bitrate' => 300],
         ];
     }
-    private static function saveQualities($qualities,$video,$compressedFilePath,$name){
+
+    private static function saveQualities($qualities, $video, $compressedFilePath, $name)
+    {
         foreach ($qualities as $quality => $settings) {
             $format = new X264('aac', 'libx264');
             $format->setVideoCodec('libx264')
@@ -38,55 +41,64 @@ class VideoQualities
                     '-r', '15',             // Lower frame rate for smaller size
                 ]);
 
-
             $video->filters()->resize(new Dimension($settings['width'], $settings['height']));
-            $path = $compressedFilePath."/".$name."_".$quality.".mp4";
+            $path = $compressedFilePath . "/" . $name . "_" . $quality . ".mp4";
 
-            if(env('WAS_STATUS') == 1){
-                // save at wasbi
-                self::save_at_wasabi($name,$path);
-            }else{
+            if (env('WAS_STATUS') == 1) {
+                // save at wasabi
+                self::save_at_wasabi($name, $path);
+            } else {
                 $video->save($format, $path);
             }
             // Delete the temporary compressed file
             //unlink($path);
-
         }
     }
 
-    public static function save_videos_using_commands($video_obj,$compressedFilePath,$name)
+    public static function save_videos_using_commands($video_obj, $compressedFilePath, $name)
     {
         self::$final_names = [];
         $items = [
-          [
-            'quality'=>'144',
-            'scale'=>'-vf scale=-1:144',
-          ],
-          [
-            'quality'=>'360',
-            'scale'=>'-vf scale=640:-1',
-          ],
-          [
-            'quality'=>'original',
-            'scale'=>'',
-          ],
+            [
+                'quality' => '144',
+                'scale' => '-vf scale=-1:144',
+            ],
+            [
+                'quality' => '360',
+                'scale' => '-vf scale=640:-1',
+            ],
+            [
+                'quality' => 'original',
+                'scale' => '',
+            ],
         ];
 
-        foreach ($items as $item){
-            $file_name = $name."-".$item['quality'].".mp4";
+        foreach ($items as $item) {
+            $file_name = $name . "-" . $item['quality'] . ".mp4";
+            $output = [];
+            $return_var = 0;
 
-            exec("ffmpeg -i $video_obj ".$item['scale']." $compressedFilePath$file_name");
-            self::save_at_wasabi($file_name,$compressedFilePath.$file_name);
-            array_push(self::$final_names,['quality'=>$item['quality'],'name'=>$file_name]);
+            exec("ffmpeg -i $video_obj " . $item['scale'] . " $compressedFilePath$file_name 2>&1", $output, $return_var);
 
+            // Log the output and return status
+            Log::info("FFmpeg output for {$item['quality']}: " . implode("\n", $output));
+            Log::info("FFmpeg return status for {$item['quality']}: $return_var");
+
+            if ($return_var === 0) {
+                self::save_at_wasabi($file_name, $compressedFilePath . $file_name);
+                array_push(self::$final_names, ['quality' => $item['quality'], 'name' => $file_name]);
+            } else {
+                Log::error("Failed to process quality {$item['quality']} for video $name");
+            }
+
+            // Add a small delay between commands
+            sleep(2);
         }
-
     }
 
-    public static function save_at_wasabi($name,$compressedFilePath,$start_path_wasbi = 'videos')
+    public static function save_at_wasabi($name, $compressedFilePath, $start_path_wasbi = 'videos')
     {
-
-        if(env('WAS_STATUS') == 1){
+        if (env('WAS_STATUS') == 1) {
             // Set up AWS S3 Client with Wasabi credentials
             $s3Client = new S3Client([
                 'version' => 'latest',
@@ -97,53 +109,45 @@ class VideoQualities
                     'secret' => env('WAS_SECRET_ACCESS_KEY'),
                 ],
             ]);
-            //dd($start_path_wasbi.'/'. $name,$compressedFilePath);
+
             // Upload the compressed video to Wasabi
             $result = $s3Client->putObject([
                 'Bucket' => env('WAS_BUCKET'),
-                'Key'    => $start_path_wasbi.'/'. $name,
+                'Key'    => $start_path_wasbi . '/' . $name,
                 'SourceFile' => $compressedFilePath,
                 'ACL'    => 'public-read',
             ]);
         }
-
-
     }
-    public static function execute($file,$name,$is_command = true)
+
+    public static function execute($file, $name, $is_command = true)
     {
         // Define the paths
-
         $originalFilePath = $file->getRealPath();
         $compressedFilePath = storage_path('app/tmp/');
         $filePath = $file->getRealPath();
 
-        if($is_command){
-            self::save_videos_using_commands($file,$compressedFilePath,$name);
-            if(env('WAS_STATUS') == 1){
+        if ($is_command) {
+            self::save_videos_using_commands($file, $compressedFilePath, $name);
+            if (env('WAS_STATUS') == 1) {
                 File::cleanDirectory($compressedFilePath);
             }
             return 1;
         }
+
         $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => env('APP_ENV') == 'local'?'C:/ffmpeg/ffmpeg.exe':'/usr/bin/ffmpeg',
-            'ffprobe.binaries' => env('APP_ENV') == 'local'?'C:/ffmpeg/ffprobe.exe':'/usr/bin/ffprobe',
+            'ffmpeg.binaries'  => env('APP_ENV') == 'local' ? 'C:/ffmpeg/ffmpeg.exe' : '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => env('APP_ENV') == 'local' ? 'C:/ffmpeg/ffprobe.exe' : '/usr/bin/ffprobe',
             'timeout'          => 3600, // seconds
             'ffmpeg.threads'   => 12,
         ]);
 
-
-
         $video = $ffmpeg->open($filePath);
-
-
 
         // Quality Settings
         $qualities = self::getQualities();
 
         // save qualities
-        self::saveQualities($qualities,$video,$compressedFilePath,$name);
-
-
-
+        self::saveQualities($qualities, $video, $compressedFilePath, $name);
     }
 }
